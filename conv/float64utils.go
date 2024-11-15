52d3e6cb0f34a3631cf4139f42355bb6876c8efe
Ермаков Дмитрий <dermakov@croc.ru>
2024-11-15 13:06:47 +0300

    Добавлена функция Sub

--- conv/float64utils.go
+++ conv/float64utils.go
@@ -9,3 +9,13 @@ func Add(numbers ...float64) float64 {
 
 	return sum
 }
+
+func Sub(minuend float64, subtrahends ...float64) float64 {
+	var result float64 = minuend
+
+	for _, subtrahend := range subtrahends {
+		result -= subtrahend
+	}
+
+	return result
+}
